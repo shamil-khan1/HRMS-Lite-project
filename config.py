5da10805128bc3c99d05d7dbf394ab3b8fda37d7
shamil-khan1 <shamiljaikam@gmail.com>
2026-02-11 12:17:12 +0530

    render deploy fix

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
+import os
 class Config:
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root123@localhost:3307/hrms_db"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("postgresql://hrms_db_ibmt_user:OkUtBcxDTnaupZ8s2aapUtepywy28sXv@dpg-d661otfgi27c73djomcg-a/hrms_db_ibmt")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
